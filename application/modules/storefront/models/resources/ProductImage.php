e8c189bfa23a900761befc6cb687dc6b49ff1c0b
Mark Johnson <luther07@gmail.com>
2012-03-12 13:53:16 -0500

    finishing up autoloader and category controller

--- application/modules/storefront/models/resources/ProductImage.php
+++ application/modules/storefront/models/resources/ProductImage.php
@@ -1,5 +1,13 @@
 <?php
 
+//I don't know if our autoloader will automatically load classes that Zend_Db_Table needs (dependencies)
+if (!class_exists('Storefront_Resource_ProductImage_Item')) {
+    require_once dirname(__FILE__) . '/ProductImage/Item.php' ;
+}
+if (!class_exists('Storefront_Resource_Product')) {
+    require_once dirname(__FILE__) . '/Product.php' ;
+}
+
 class Storefront_Resource_ProductImage extends SF_Model_Resource_Db_Table_Abstract implements Storefront_Resource_ProductImage_Interface{
     protected $_name = 'productImage' ;
     protected $_primary = 'imageId' ;
